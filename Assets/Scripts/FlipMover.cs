729d90b25022d4bed748140e1ca4128cd5c4e49c
Gallae <71407574+Gallae@users.noreply.github.com>
2025-07-22 19:41:56 -0400

    Health and Stamina Bars
    Added assets and logic for a health bar and stamina bar for FlipMover object

--- Assets/Scripts/FlipMover.cs
+++ Assets/Scripts/FlipMover.cs
@@ -1,17 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlipMover : MonoBehaviour
 {
+    public AudioSource moveClickAudioSource;
+    public AudioSource stopClickAudioSource;
+    public AudioSource flipClickAudioSource;
+
+    public AudioClip stopClickAudioClip;
+
+    public List<AudioClip> flipClickAudioClips;
+
+    public Slider staminaBar;
+    public float maxStamina = 100;
+    public float minStamina;
+    public float staminaLoss = 10f;
+
+    private float currentStamina;
+
     bool moveRight = false;
-    float speed = 1f;
+    private float speed = 1f;
+    private int direction = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        stopClickAudioSource.clip = stopClickAudioClip;
+        currentStamina = maxStamina;
+        staminaBar.value = currentStamina / maxStamina;
     }
 
     // Update is called once per frame
@@ -19,24 +40,48 @@ public class FlipMover : MonoBehaviour
     {
         if (moveRight)
         {
-            Vector3 newPosition = transform.position + Vector3.right * (speed*Time.deltaTime);
-            transform.position = newPosition;
+            transform.position += Vector3.right * (speed*direction*Time.deltaTime);
+
+            currentStamina -= staminaLoss *Time.deltaTime;
+            staminaBar.value = currentStamina / maxStamina;
+            if (currentStamina <= 0)
+            {
+                speed = 0.1f;
+            }
+        }
+        else
+        {
+            currentStamina += staminaLoss * Time.deltaTime * 2;
+            staminaBar.value = currentStamina / maxStamina;
+            speed = 1f;
+            if(currentStamina > 100)
+            {
+                currentStamina = 100;
+            }
         }
     }
 
     public void OnMoveClick()
     {
         moveRight = true;
+        moveClickAudioSource.Play();
     }
 
     public void OnStopClick()
     {
         moveRight = false;
+        stopClickAudioSource.Play();
     }
     
     public void OnFlipClick()
     {
-        speed *= -1;
+        direction *= -1;
+
+        //take our clips and choose one randomly
+        int randomIndex = UnityEngine.Random.Range(0, flipClickAudioClips.Count);
+        AudioClip randomlyChosenClip = flipClickAudioClips[randomIndex];
+
+        flipClickAudioSource.PlayOneShot(randomlyChosenClip);
     }
 
     public void OnFastClick()
